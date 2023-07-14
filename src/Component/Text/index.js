e9591818741e70199a59c65fe4237698a576ac5b
Utkarsh Srivastava <utksrivastava18@gmail.com>
2023-07-14 17:10:24 +0530

    COmmit regarding css and fontawesome

--- src/Component/Text/index.js
+++ src/Component/Text/index.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import './text.css'
+import { Col, Row } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun } from '@fortawesome/free-solid-svg-icons';
 
+const Text = () => {
+   return (
+      <>
 
+         <div className="middleNew">
+            <Row>
+               <div className="">
+                  <Col className="revolution" span={24}>Revolutionize Creativity</Col>
+               </div>
+            </Row>
+            <Row className="revolution">
+               <Col span={12}>Unleash the power of ChatGPT clone’s AI-driven text generator! It’s time to give your content the x-factor that’ll make it stand out </Col>
+               <Col span={12}>Capture your audience’s imagination with breathtaking image generation. Let our state-of-the-art AI paint the perfect image for you.</Col>
+            </Row>
 
-const Text=()=>{
-    return(
-        <>
-          <div className="middle">Revolutionize Creativity
-         < br/>
-         <div className="unlease">
-            <p >
-            Unleash the power of ChatGPT clone’s AI-driven text generator! It’s time to give your content the x-factor that’ll make it stand out 
-            </p>
+            <Row className='imageRow'>
+               <div>
+                  <FontAwesomeIcon icon={faSun} spin size="2xl" style={{ color: "#fff700" }} />
+               </div>
+            </Row>
          </div>
-         <div className="capture">
-            <p>
-            Capture your audience’s imagination with breathtaking image generation. Let our state-of-the-art AI paint the perfect image for you.
-            </p>
-         </div>
-          </div>
-          {/* <div className="">Unleash the power of ChatGPT clone’s AI-driven text generator! It’s time to give your content the x-factor that’ll make it stand out.</div> */}
-          
-         
-        </>
 
- 
- 
+      </>
 
-    )
-  };
-  export default Text;
+   )
+};
+export default Text;
