92d11b4e57f50316b9a1be39aa0df3f1f63cc708
sakshisingh301 <sakshisingh57@deloitte.com>
2023-07-21 15:44:39 +0530

    size modification

--- src/Component/Header/index.js
+++ src/Component/Header/index.js
@@ -14,15 +14,15 @@ const Header = () => {
             <div style={{
                 margin: '10px', fontSize: '2.5em', fontFamily: 'ui-monospace', fontStyle: 'oblique', float: 'left'
             }}>ChitterBitter</div>
-            <div style={{ fontSize: '1em', float: 'left', margin:'25px' }}> <Link to="/">Home</Link></div>
-           
-            <div style={{ fontSize: '1em', float: 'left', marginTop: '25px'  }}><Link to="/text">Text-To-Text</Link></div>
-            
-            <div style={{ fontSize: '1em', float: 'left', margin: '25px'  }}><Link to="/image">Text-To-Image</Link></div>
-            
-            <div style={{ fontSize: '1em', float: 'left', marginTop:'25px'  }}><Link to="/searchPrompt">Search Prompt</Link></div>
-            
-            <div style={{float: 'right', marginTop: '15px'}}>
+            <div style={{ fontSize: '20px', float: 'left', margin: '25px' ,color:'antiquewhite'}}> <Link to="/">Home</Link></div>
+
+            <div style={{ fontSize: '20px', float: 'left', marginTop: '25px' }}><Link to="/text">Text-To-Text</Link></div>
+
+            <div style={{ fontSize: '20px', float: 'left', margin: '25px' }}><Link to="/image">Text-To-Image</Link></div>
+
+            <div style={{ fontSize: '20px', float: 'left', marginTop: '25px' }}><Link to="/searchPrompt">Search Prompt</Link></div>
+
+            <div style={{ float: 'right', marginTop: '15px' }}>
                 <FontAwesomeIcon icon={faSignOutAlt} size="2xl" />
             </div>
         </header>
