dff22ab64472d8b8ec76f40ded1a174bd2502406
sakshisingh57 <sakshisingh57@deloitte.com>
2023-09-12 17:22:05 +0530

    breadcrumps modification

--- src/Component/BreadCrump/index.js
+++ src/Component/BreadCrump/index.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import { Breadcrumb } from 'antd';
-import './bread.css'
+import { Link, useLocation } from 'react-router-dom';
+import './bread.css';
 
-const Bread = () => (
-  <Breadcrumb
-    separator=">"
-    items={[
-      {
-        title: 'Home',
-        href:'http://localhost:3000'
-      },
-      {
-        title: 'text to text',
-        href: 'http://localhost:3000/text',
-      },
-      {
-        title: 'text to image',
-        href: 'http://localhost:3000/image',
-      },
-    ]}
-  />
-);
-  export default Bread;
+const Bread = () => {
+  const location = useLocation();
+
+  const pathParts = location.pathname.split('/').filter(Boolean);
+
+  // Define a style object for the breadcrumb
+  const breadcrumbStyle = {
+    color: 'white', // Set the font color to white
+  };
+
+  return (
+    <Breadcrumb separator=">" style={breadcrumbStyle}>
+      <Breadcrumb.Item key="home">
+        <Link to="/">Home</Link>
+      </Breadcrumb.Item>
+      {pathParts.map((part, index) => (
+        <Breadcrumb.Item key={index}>
+          <Link to={`/${pathParts.slice(0, index + 1).join('/')}`}>
+            {part.charAt(0).toUpperCase() + part.slice(1).replace(/-/g, ' ')}
+          </Link>
+        </Breadcrumb.Item>
+      ))}
+    </Breadcrumb>
+  );
+};
+
+export default Bread;
